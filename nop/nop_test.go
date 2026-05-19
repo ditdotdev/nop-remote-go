1d6492d07fe6a5ecb4cf6ca7798749b354d77ae6
Robert Reeves <robertericreeves@gmail.com>
2026-05-19 10:35:51 -0500

    Adapt to remote-sdk-go Get signature change (#46)
    * Adapt to remote-sdk-go Get signature change
    
    remote-sdk-go PR datadatdat/remote-sdk-go#49 changes Get(string) to
    return (Remote, bool) instead of just Remote (fixes #46 finding #4).
    
    The bool is always true for these tests because each provider's init()
    registers itself, so the migration is mechanical: every
    "r := remote.Get(...)" becomes "r, _ := remote.Get(...)". No behavior
    change.
    
    SDK dependency is pinned to a pseudoversion of the SDK branch; will
    switch to v1.10.0 once the SDK PR merges and tags.
    
    * Pin remote-sdk-go to v1.10.0 (released)

--- nop/nop_test.go
+++ nop/nop_test.go
@@ -11,13 +11,13 @@ import (
 )
 
 func TestRegistered(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	ret, _ := r.Type()
 	assert.Equal(t, "nop", ret)
 }
 
 func TestFromURL(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	res, err := r.FromURL("nop", map[string]string{})
 
 	if assert.NoError(t, err) {
@@ -27,25 +27,25 @@ func TestFromURL(t *testing.T) {
 }
 
 func TestBadUrl(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	_, err := r.FromURL("not\nurl", map[string]string{})
 	assert.Error(t, err)
 }
 
 func TestBadAuthority(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	_, err := r.FromURL("nop://foo", map[string]string{})
 	assert.Error(t, err)
 }
 
 func TestBadProperty(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	_, err := r.FromURL("nop", map[string]string{"a": "b"})
 	assert.Error(t, err)
 }
 
 func TestToURL(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	u, props, err := r.ToURL(map[string]interface{}{})
 
 	if assert.NoError(t, err) {
@@ -55,7 +55,7 @@ func TestToURL(t *testing.T) {
 }
 
 func TestGetParameters(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	res, err := r.GetParameters(map[string]interface{}{})
 
 	if assert.NoError(t, err) {
@@ -64,31 +64,31 @@ func TestGetParameters(t *testing.T) {
 }
 
 func TestValidateRemoteSuccess(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	err := r.ValidateRemote(map[string]interface{}{})
 	assert.NoError(t, err)
 }
 
 func TestValidateRemoteFailure(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	err := r.ValidateRemote(map[string]interface{}{"a": "b"})
 	assert.Error(t, err)
 }
 
 func TestValidateParametersSuccess(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	err := r.ValidateParameters(map[string]interface{}{})
 	assert.NoError(t, err)
 }
 
 func TestValidateParametersFailure(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	err := r.ValidateParameters(map[string]interface{}{"a": "b"})
 	assert.Error(t, err)
 }
 
 func TestListCommits(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	res, err := r.ListCommits(map[string]interface{}{}, map[string]interface{}{}, []remote.Tag{})
 
 	if assert.NoError(t, err) {
@@ -97,7 +97,7 @@ func TestListCommits(t *testing.T) {
 }
 
 func TestGetCommit(t *testing.T) {
-	r := remote.Get("nop")
+	r, _ := remote.Get("nop")
 	res, err := r.GetCommit(map[string]interface{}{}, map[string]interface{}{}, "id")
 
 	if assert.NoError(t, err) {
